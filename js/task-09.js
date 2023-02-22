49859ae118d808ed76ff302b92097d8c795c8588
Oleksandr Siukov <as-service2008@ukr.net>
2023-02-22 22:10:16 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  onChangeColor: document.querySelector("button.change-color"),
+  infoChangeColor: document.querySelector("span.color"),
+};
+
+const changeBackgroundColor = () => {
+  const randomHexColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomHexColor;
+  refs.infoChangeColor.textContent = randomHexColor;
+};
+
+refs.onChangeColor.addEventListener("click", changeBackgroundColor);
