37815f202ac37e326c679a48660d382df438e522
Oleksandr Siukov <as-service2008@ukr.net>
2023-02-23 21:41:28 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Please fill in all fields!");
+  } else {
+    const formData = { email, password };
+    console.log(formData);
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", onFormSubmit);
