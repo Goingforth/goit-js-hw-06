f99707fe6b3d0f35271cbb0ecb1ff586957ae82c
Oleksandr Siukov <as-service2008@ukr.net>
2023-02-25 19:48:46 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  number: document.querySelector("input"),
+  onBtnCreate: document.querySelector("button[data-create]"),
+  onBtnDestroy: document.querySelector("button[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+const createBoxes = () => {
+  let amount = Number(refs.number.value);
+  const newBoxes = [];
+  let baseSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const newBox = document.createElement("div");
+
+    newBox.style.width = `${baseSize}px`;
+    newBox.style.height = `${baseSize}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+
+    newBoxes.push(newBox);
+
+    baseSize += 10;
+  }
+
+  refs.boxes.append(...newBoxes);
+};
+
+const destroyBoxes = () => {
+  while (refs.boxes.firstChild) {
+    refs.boxes.removeChild(boxes.firstChild);
+  }
+  refs.number.value = "";
+};
+
+refs.onBtnCreate.addEventListener("click", createBoxes);
+
+refs.onBtnDestroy.addEventListener("click", destroyBoxes);
